cbe2e20f9b4dc3e8ad6df9272c73daa68df4d83f
Desdevises Simon <simon.desdevises@gmail.com>
2023-08-06 23:46:56 +0200

    delete useless files

--- frontend/src/API/images_information.ts
+++
@@ -1,9 +0,0 @@
-const cleAPI = process.env.REACT_APP_API_URL;
-
-export default async function images_information(
-  number: number
-): Promise<[number][]> {
-  const res = await fetch(`${cleAPI}/api/images?num=${number}`);
-  const data = await res.json();
-  return data;
-}
